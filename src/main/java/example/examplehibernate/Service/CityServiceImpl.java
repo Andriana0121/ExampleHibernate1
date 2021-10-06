5c66435371c9fe18976a5776f443d74b6a448aa0
Andriana0121 <vorotna.andriana01@gmail.com>
2021-10-06 23:37:32 +0300

    update

--- src/main/java/example/examplehibernate/Service/CityServiceImpl.java
+++ src/main/java/example/examplehibernate/Service/CityServiceImpl.java
@@ -1,15 +1,9 @@
 package example.examplehibernate.Service;
 
-import com.sun.deploy.security.JarSignature;
 import example.examplehibernate.Exception.NotFoundException;
 import example.examplehibernate.Model.City;
 import example.examplehibernate.Repository.CityRepository;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
