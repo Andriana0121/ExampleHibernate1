71adaf67c44201d386431fec9bd1a22d95184e7a
Andriana0121 <vorotna.andriana01@gmail.com>
2021-10-06 21:13:45 +0300

    add save method into CountryServiceImpl

--- src/main/java/example/examplehibernate/Service/CountryServiceImpl.java
+++ src/main/java/example/examplehibernate/Service/CountryServiceImpl.java
@@ -1,4 +1,20 @@
 package example.examplehibernate.Service;
 
-public class CountryServiceImpl {
+import example.examplehibernate.Model.Country;
+import example.examplehibernate.Repository.CountryRepository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CountryServiceImpl implements CountryService {
+    CountryRepository countryRepository;
+
+    public CountryServiceImpl(CountryRepository countryRepository) {
+        super();
+        this.countryRepository = countryRepository;
+    }
+
+    @Override
+    public Country save(Country country) {
+        return countryRepository.save(country);
+    }
 }
