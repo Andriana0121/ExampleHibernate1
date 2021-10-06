3c64b522dc0ce6e6597c3852a0771b095d96ec44
Andriana0121 <vorotna.andriana01@gmail.com>
2021-10-06 19:48:21 +0300

    Initial commit

---
+++ src/main/java/example/examplehibernate/ExampleHibernateApplication.java
@@ -0,0 +1,13 @@
+package example.examplehibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExampleHibernateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/example/examplehibernate/ExampleHibernateApplicationTests.java
@@ -0,0 +1,13 @@
+package example.examplehibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExampleHibernateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
