3b092d70d9f22519079b8adad80b6d9834a0e1f2
Andriana0121 <vorotna.andriana01@gmail.com>
2021-10-06 19:53:48 +0300

    add CountryController

---
+++ src/main/java/example/examplehibernate/Controller/CountryController.java
@@ -0,0 +1,10 @@
+package example.examplehibernate.Controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/countries")
+public class CountryController {
+
+}
