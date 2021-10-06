311d18ec5cc115c9a1aac3b7e6b148638b94744c
Andriana0121 <vorotna.andriana01@gmail.com>
2021-10-06 20:08:23 +0300

    add data/entity/table

--- src/main/java/example/examplehibernate/Model/Country.java
+++ src/main/java/example/examplehibernate/Model/Country.java
@@ -1,4 +1,19 @@
 package example.examplehibernate.Model;
 
+import  lombok.Data ;
+import javax.persistence.*;
+
+@Data
+@Entity
+@Table(name = "countries")
 public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "code")
+    private String code;
 }
