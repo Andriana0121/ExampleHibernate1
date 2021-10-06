e457f20605a26adef293a5ab9ac49c89dadc767c
Andriana0121 <vorotna.andriana01@gmail.com>
2021-10-06 21:00:51 +0300

    add create method and add save method into CountryService

--- src/main/java/example/examplehibernate/Controller/CountryController.java
+++ src/main/java/example/examplehibernate/Controller/CountryController.java
@@ -1,10 +1,23 @@
 package example.examplehibernate.Controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import example.examplehibernate.Model.Country;
+import example.examplehibernate.Service.CountryService;
+import org.springframework.http.*;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/countries")
 public class CountryController {
+    private CountryService countryService;
+
+    public CountryController(CountryService countryService) {
+        super();
+        this.countryService = countryService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Country> create(@RequestBody Country country) {
+        return new ResponseEntity<Country>(countryService.save(country), HttpStatus.CREATED);
+    }
 
 }
--- src/main/java/example/examplehibernate/Service/CountryService.java
+++ src/main/java/example/examplehibernate/Service/CountryService.java
@@ -1,4 +1,7 @@
 package example.examplehibernate.Service;
 
+import example.examplehibernate.Model.Country;
+
 public interface CountryService {
+    Country save(Country country);
 }
